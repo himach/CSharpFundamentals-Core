f6c4c7375b0e5bfdf21cc505abf3be4f5e1a1b71
himach <himacharya@gmail.com>
2021-04-11 15:47:38 -0500

    Create Initial solution

---
+++ Fundamental_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamental_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
