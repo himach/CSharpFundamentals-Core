94e006f66afa231f33e0f256c06f0942116bb038
himach <himacharya@gmail.com>
2021-04-12 23:44:59 -0500

    Use of list

--- Fundamental_1/Program.cs
+++ Fundamental_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Fundamental_1
 {
@@ -6,7 +7,14 @@ namespace Fundamental_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> data = new List<int>{12,11,1,5,0 };
+            data.Add(19);
+            int sum = 0;
+            foreach (var number in data)
+            {
+                sum += number;
+            }
+            Console.WriteLine("sum of the list is {0}", sum);
         }
     }
 }
